880a7519f67be26abbc7eed5e15ac110ea0d16b7
Virkki Inkeri A <vbinkeri@lx7-fuxi352.ad.helsinki.fi>
2018-06-06 14:01:12 +0300

    poisto funktio päivitys

--- application/lists/views.py
+++ application/lists/views.py
@@ -16,7 +16,7 @@ def lists_form():
 	return render_template("lists/new.html", form = ListForm())
 
 #listan poisto
-@app.route("/lists/delete/<list_id>")
+@app.route("/lists/delete/<list_id>", methods=["POST"])
 @login_required
 def lists_delete(list_id):
 	list = List.query.get(list_id)

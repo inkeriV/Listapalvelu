ace953435a2438898a778696dd164ad2f67f53fe
Virkki Inkeri A <vbinkeri@lx7-fuxi352.ad.helsinki.fi>
2018-06-06 12:54:43 +0300

    sulku puttui

--- application/lists/views.py
+++ application/lists/views.py
@@ -16,7 +16,7 @@ def lists_form():
 	return render_template("lists/new.html", form = ListForm())
 
 #listan poisto
-@app.route("/lists/delete/<list_id>", methods=["POST"]
+@app.route("/lists/delete/<list_id>", methods=["POST"])
 @login_required
 def lists_delete():
 	#db = get_db()

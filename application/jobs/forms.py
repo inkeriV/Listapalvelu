2d07453a20bf49c508ec345d6eba5dd98fe8e6dc
Virkki Inkeri A <vbinkeri@lx7-fuxi352.ad.helsinki.fi>
2018-06-18 09:55:50 +0300

    lomakkeiden validointiehtojen lisäyksiä

--- application/jobs/forms.py
+++ application/jobs/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, IntegerField
+from wtforms.validators import DataRequired, Length
 
 class JobForm(FlaskForm):
-	name = StringField("Job", [validators.Length(min=3)])
+	name = StringField("Job", validators=[DataRequired("anna työlle nimi"), Length(min=1, max=100)])
 	status = IntegerField("1")
 
 	class Meta:
--- application/lists/forms.py
+++ application/lists/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, IntegerField
+from wtforms.validators import DataRequired, Length
 
 class ListForm(FlaskForm):
-	name = StringField("List name", [validators.Length(min=2)])
+	name = StringField("List name", validators=[DataRequired("anna listalle nimi"), Length(min=1, max=30)])
 	type = IntegerField("1")
 
 	class Meta:

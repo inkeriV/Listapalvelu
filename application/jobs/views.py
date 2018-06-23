5b55804a0079cc0150975424ea0549e666cc3392
Virkki Inkeri A <vbinkeri@lx7-fuxi352.ad.helsinki.fi>
2018-06-23 16:18:24 +0300

    flash viestit näkyviin

--- application/jobs/views.py
+++ application/jobs/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 from application import app, db
